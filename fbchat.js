14f99091ce00fba6d1c700f0e0c532d9de5286fc
Henry Elliott <henrylovesscience@gmail.com>
2016-12-08 11:32:15 -0600

    Add license

--- fbchat.js
+++ fbchat.js
@@ -1,8 +1,28 @@
 #!/usr/bin/env node
 /*
- * Copyright 2016 Henry Elliott
  * MIT License
  *
+ * Copyright (c) 2016 Henry Elliott
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/*
  * Developed with node version 6.9.1
  */
 'use strict';
@@ -67,7 +87,7 @@ login(JSON.parse(config), (err, api) => {
 
   api.listen((error, message) => {
     api.getUserInfo(message.senderID, (err, obj) => {
-      console.log(obj[message.senderID].name + ": " + message.body);
+      console.log('\n' + obj[message.senderID].name + ': ' + message.body);
       rl.prompt();
     });
   });
