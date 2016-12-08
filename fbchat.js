ca67cf287bda3a9c121f2d65cbe2765815fd9041
Henry Elliott <henrylovesscience@gmail.com>
2016-12-08 00:42:46 -0600

    Get chat program working.

--- fbchat.js
+++ fbchat.js
@@ -18,25 +18,51 @@ login(JSON.parse(config), (err, api) => {
     return console.error(err);
   }
 
-  api.setOptions({selfListen: true});
+  api.setOptions({logLevel: "silent"});
 
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: 'fbchat> '
+    prompt: 'fbchat$ '
   });
 
   rl.prompt();
+
+  let currentID = null;
 
   rl.on('line', (line) => {
     //api.sendMessage({body: line}, currid);
     if (line.length !== 0) {
       if (line[0] === '/') {
-        const cmdArr = line.substr(1).split(/\s+/);
+        const cmdArr = line.substr(1).trim().split(/\s+/);
+        switch (cmdArr[0]) {
+        case "join":
+          //console.log("Joining: cmdArr.length === " + cmdArr.length);
+          let name = "";
+          for (let i = 1; i < cmdArr.length; i++) {
+            name += (i !== cmdArr.length - 1) ? (cmdArr[i] + " ") : (cmdArr[i]);
+          }
+          api.getUserID(name, (err, data) => {
+            // Handle error.
+            currentID = data[0].userID;
+            rl.setPrompt('fbchat:' + name + '$ ');
+            rl.prompt();
+          });
+          //console.log(name);
+          break;
+        case "quit":
+        case "exit":
+          process.exit();
+        }
+        //console.log("cmdArr[0] === " + cmdArr[0]);
+        rl.prompt();
       } else {
+        api.sendMessage(line, currentID);
+        rl.prompt();
       }
+    } else {
+      rl.prompt();
     }
-    rl.prompt();
   });
 
   api.listen((error, message) => {
